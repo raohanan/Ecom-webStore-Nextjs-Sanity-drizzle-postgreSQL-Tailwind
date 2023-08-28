362378e079c26db0f9b477313a75e926b3c3f487
raohanan <ad.hanan01@gmail.com>
2023-08-28 05:40:37 -0700

    fixed some errors

--- src/components/shared/BasePath.ts
+++ src/components/shared/BasePath.ts
@@ -1,4 +1,10 @@
-const BASE_PATH_FORAPI = process.env.NODE_ENV==="development"? 
-"http://localhost:3000"
-: "https://ecom-store.vercel.app"
+// const BASE_PATH_FORAPI = process.env.NODE_ENV==="development"? 
+// "http://localhost:3000"
+// : "https://ecom-store.vercel.app"
+// export default BASE_PATH_FORAPI
+
+
+const BASE_PATH_FORAPI = "http://localhost:3000"
+
+
 export default BASE_PATH_FORAPI
